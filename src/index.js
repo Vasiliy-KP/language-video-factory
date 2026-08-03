3d0c8ac6032844b09233c331c717703f39a6a3ba
Vasiliy-Kp <Ukrainepochta2013@gmail.com>
2026-08-03 17:55:52 +0300

    feat: add word validation pipeline

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import { config } from "./config/config.js";
-import { logger } from "./utils/logger.js";
-import { ExcelReader } from "./data/excelReader.js";
-import { WordValidator } from "./validators/wordValidator.js";
+import { Pipeline } from "./pipeline/Pipeline.js";
 
 console.clear();
 
+const pipeline = new Pipeline();
 
-
-logger.success(config.app.name);
-logger.info(`Version: ${config.app.version}`);
-
-const reader = new ExcelReader();
-
-const words = await reader.read();
-
-const validWords = words.filter(word => WordValidator.validate(word));
-
-logger.success(`Valid words: ${validWords.length}`);
-
-console.table(validWords);
+await pipeline.run();
---
+++ src/pipeline/pipeline.js
@@ -0,0 +1,27 @@
+import { config } from "../config/config.js";
+import { logger } from "../utils/logger.js";
+import { ExcelReader } from "../data/excelReader.js";
+import { WordValidator } from "../validators/wordValidator.js";
+
+export class Pipeline {
+
+    async run() {
+
+        logger.success(config.app.name);
+        logger.info(`Version: ${config.app.version}`);
+
+        const reader = new ExcelReader();
+
+        const words = await reader.read();
+
+        const validWords = words.filter(word =>
+            WordValidator.validate(word)
+        );
+
+        logger.success(`Valid words: ${validWords.length}`);
+
+        console.table(validWords);
+
+    }
+
+}
